364f1f4785c9cafeaad026dbb099d7f2e7a9c779
José Alves <dev@alvesjose.com.br>
2021-07-18 11:10:07 -0300

    FEAT: add seeds files

---
+++ src/database/seeds/20210718140328-admin-user.js
@@ -0,0 +1,20 @@
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+  up: async (queryInterface) => {
+    await queryInterface.bulkInsert(
+      'users',
+      [{
+        name: 'Distribuidora FastFeet',
+        email: 'admin@fastfeet.com',
+        password_hash: bcrypt.hashSync('123456', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+
+      }], {},
+    );
+  },
+
+  down: async () => {
+  },
+};
