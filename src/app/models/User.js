7e136c2f9bd502f8fd0a88b1774507e7f645eaa1
José Alves <dev@alvesjose.com.br>
2021-07-18 11:28:51 -0300

    FEAT: add database configurations and hook to password_hash

---
+++ src/app/models/User.js
@@ -0,0 +1,25 @@
+import Sequelize, { Model } from 'sequelize';
+import brcypt from 'bcryptjs';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init({
+      name: Sequelize.STRING,
+      email: Sequelize.STRING,
+      password: sequelize.VIRTUAL,
+      password_hash: Sequelize.STRING,
+    },
+    {
+      sequelize,
+    });
+
+    this.addHook('beforeSave', async (user) => {
+      if (user.password) {
+        user.password_hash = await brcypt.hash(user.password, 8);
+      }
+    });
+    return this;
+  }
+}
+
+export default User;
