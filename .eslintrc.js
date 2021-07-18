f4dff56db539f0537f89712d907acc002cb18d87
José Alves <dev@alvesjose.com.br>
2021-07-18 10:54:20 -0300

    FEAT: prepare squelize

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,6 @@ module.exports = {
     'class-methods-use-this': 'off',
     'no-param-reassign': 'off',
     camelcase: 'off',
-    'no-unsude-vars': ['error', { argsIgnorePattern: 'next' }],
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
   },
 };
