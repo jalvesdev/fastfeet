7f9f0f3756515421998677684bba6fc339fa75c7
José Alves <dev@alvesjose.com.br>
2021-07-18 10:20:46 -0300

    FEAT: prepare structure files

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'class-methods-use-this': 'off',
+    'no-param-reassign': 'off',
+    camelcase: 'off',
+    'no-unsude-vars': ['error', { argsIgnorePattern: 'next' }],
+  },
+};
